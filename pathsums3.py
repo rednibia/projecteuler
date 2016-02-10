6dfe13fb9796b20670d3c9104173817a1cc06472
rednibia <andrewaibinder@gmail.com>
2016-02-10 14:41:42 -0500

    Update pathsums3.py

--- pathsums3.py
+++ pathsums3.py
@@ -1,3 +1,9 @@
+"""
+The minimal path sum in the 5 by 5 matrix below, by starting in any cell in the left column and finishing in any cell in the right column, and only moving up, down, and right, is indicated in red and bold; the sum is equal to 994.
+Find the minimal path sum, in a 80 by 80 matrix, from the left column to the right column.
+"""
+
+
 def decide(grid, cursorx, cursory):
     global pathfound
     if cursorx == 79 and cursory == 79:
